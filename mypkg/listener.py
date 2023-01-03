d638b6e3955b9933fa872aa29b20c2c76d36d08f
aiba0921 <s21C1004GV@s.chibakoudai.jp>
2023-01-03 13:10:36 +0900

    update README and add class listener

--- mypkg/listener.py
+++ mypkg/listener.py
@@ -5,11 +5,19 @@ import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Int16
 
-def cb(msg):
-    global node
-    node.get_logger().info("Listen: %d" % msg.data)
+class Listener():
+    def __init__(self, node):
+        self.pub = node.create_subscription(Int16, "countup", self.cb, 10)
+        self.node = node
 
-rclpy.init()
-node = Node("listener")
-pub = node.create_subscription(Int16, "countup", cb, 10)
-rclpy.spin(node)
+    def cb(self, msg):
+        self.node.get_logger().info("Listen: %d" % msg.data)
+
+def main():
+    rclpy.init()
+    node = Node("listener")
+    listener = Listener(node)
+    rclpy.spin(node)
+
+if __name__== '__main__':
+    main()
